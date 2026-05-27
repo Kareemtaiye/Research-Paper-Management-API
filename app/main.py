7ba528cadc1db1f7ee408d78853d0f9ae13506f6
Kareem Taiye <olayinkahassan117@gmail.com>
2026-05-27 08:33:24 +0100

    add Promethues metrics, grafanna dashboards, structured logging

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 from app.core.database import lifespan
 from app.middleware.rate_limit import RateLimitMiddleware
-from app.middleware.request_logs import RequestLoggingMiddleware
+from app.middleware.request_logs import (
+    # RequestLoggingMiddleware,
+    add_logger_middleware,
+)
 from app.routers import auth, papers_import, task, user, paper, tag, websocket
 from app.exceptions.handlers import register_exception_handlers
 from app.tasks.paper_tasks import test_task
 
+from prometheus_fastapi_instrumentator import Instrumentator
+
 app = FastAPI(lifespan=lifespan)
+Instrumentator().instrument(app).expose(app)
 
 
 @app.get("/health", tags=["Health"])
@@ -21,7 +27,8 @@ def check_health():
 # cos gastapi wont call the handlers unless thy're registered or imported in this modile
 register_exception_handlers(app)
 app.add_middleware(RateLimitMiddleware)
-app.add_middleware(RequestLoggingMiddleware)
+# app.add_middleware(RequestLoggingMiddleware)
+add_logger_middleware(app)
 
 app.include_router(auth.router)
 app.include_router(user.router)
