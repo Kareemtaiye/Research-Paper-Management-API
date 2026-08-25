5df89ccbc29b6c074de88011cbf7409fffe1a50b
Kareem Taiye <olayinkahassan117@gmail.com>
2026-08-25 23:53:35 +0100

    add: db repo for task

---
+++ app/repositories/task_repo.py
@@ -0,0 +1,23 @@
+import asyncpg
+from app.core.database import with_connection
+
+
+class TaskRepository:
+    @with_connection
+    async def get_all_tasks(
+        self, conn: asyncpg.Connection, user_id: str, page: int = 1, per_page: int = 10
+    ):
+
+        offset = (page - 1) * per_page
+
+        data_query = "SELECT * FROM tasks OFFSET $1 LIMIT $2 WHERE owner_id = $3"
+        count_query = "SELECT COUNT(*) FROM tasks WHERE owner_id = $1"
+
+        data = await conn.fetch(data_query, offset, per_page, user_id)
+        count = await conn.fetchval(count_query, user_id)
+        return {"data": data, "count": count}
+
+    @with_connection
+    async def get_task_by_id(self, conn: asyncpg.Connection, task_id: str):
+        query = "SELECT * FROM tasks WHERE task_id = $1"
+        return await conn.fetchrow(query, task_id)
