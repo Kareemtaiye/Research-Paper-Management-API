4448d4d06fa12db6da6e151f5857c96e09fb9ba1
Kareem Taiye <olayinkahassan117@gmail.com>
2026-08-25 23:55:08 +0100

    add,update: task service

--- app/services/task_service.py
+++ app/services/task_service.py
@@ -1,10 +1,13 @@
 from app.core.exceptions import TaskNotFoundException
 from app.repositories.paper_repo import PaperRepository
+from app.repositories.task_repo import TaskRepository
+from app.schemas.request import ListQueryParams
 
 
 class TaskService:
     def __init__(self) -> None:
         self.paper_repo: PaperRepository = PaperRepository()
+        self.task_repo: TaskRepository = TaskRepository()
 
     async def get_paper_task_status(self, conn, task_id: str):
         paper = await self.paper_repo.get_paper_by_task_id(conn=conn, task_id=task_id)
@@ -13,3 +16,8 @@ class TaskService:
             raise TaskNotFoundException(task_id)
 
         return paper
+
+    async def get_all_tasks(self, conn, user_id: str, query_params: ListQueryParams):
+        return await self.task_repo.get_all_tasks(
+            conn=conn, user_id=user_id, **query_params.model_dump()
+        )
