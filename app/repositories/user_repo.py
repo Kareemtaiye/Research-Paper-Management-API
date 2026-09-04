13871a68bdef14a4d1fcc9e5cbc856345bc5259a
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-04 08:22:24 +0100

    update: added a update_full_name method for updating user's username, and added full_name as part of return cols when quering users from db

--- app/repositories/user_repo.py
+++ app/repositories/user_repo.py
@@ -6,17 +6,13 @@ from app.core.database import with_connection
 class UserRepository:
     @with_connection
     async def get_user_by_id(self, conn: asyncpg.Connection, id: str):
-        query = (
-            "SELECT id, email, role, is_deleted, created_at FROM users WHERE id = $1"
-        )
+        query = "SELECT id, email, full_name, role, is_deleted, created_at FROM users WHERE id = $1"
 
         return await conn.fetchrow(query, id)
 
     @with_connection
     async def get_user_by_email(self, conn: asyncpg.Connection, email: str):
-        query = (
-            "SELECT id, email, role, is_deleted, created_at FROM users WHERE email = $1"
-        )
+        query = "SELECT id, email, full_name, role, is_deleted, created_at FROM users WHERE email = $1"
 
         return await conn.fetchrow(query, email)
 
@@ -35,6 +31,15 @@ class UserRepository:
     ):
         query = "UPDATE users SET password = $1 WHERE id = $2"
         status_str = await conn.execute(query, new_password_hash, user_id)
+        operation, _, affected_row = status_str.rpartition(" ")
+        return int(affected_row)
+
+    @with_connection
+    async def update_user_full_name(
+        self, conn: asyncpg.Connection, user_id: str, new_full_name: str
+    ):
+        query = "UPDATE users SET full_name = $1 WHERE id = $2"
+        status_str = await conn.execute(query, new_full_name, user_id)
         operation, _, affected_row = status_str.rpartition(" ")
         return int(affected_row)
 
