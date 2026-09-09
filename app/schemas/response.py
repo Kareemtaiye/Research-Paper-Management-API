1b6f12bf4cb92618da31fdc5dd2611b290a6e3ca
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-09 11:25:04 +0100

    update: temp disable list page details

--- app/schemas/response.py
+++ app/schemas/response.py
@@ -5,6 +5,6 @@ from pydantic import BaseModel
 
 class ListResponse(BaseModel):  # A consistent format for all list endpoint
     data: list[Any]
-    page: int = 1
-    per_page: int = 20
-    total: int
+    # page: int = 1
+    # per_page: int = 20
+    # total: int
