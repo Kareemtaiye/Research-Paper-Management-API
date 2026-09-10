bace7dbd2a9355ba022d598906816369d29d3186
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-10 21:37:27 +0100

    add, update: updated get/update_user_pref

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,3 +1,5 @@
+import email
+
 from pydantic.v1 import EmailStr
 
 from app.core.logger import logger
@@ -17,21 +19,25 @@ class UserService:
         self.task_repo = TaskRepository()
         self.auth_repo = AuthRepository()
         self.user_preference_repo = UserPreferenceRepository()
-        
+
     async def get_user_by_id(self, conn, id: str):
         return await self.repo.get_user_by_id(conn=conn, id=id)
 
     async def get_user_by_email(self, conn, email: str):
         return await self.repo.get_user_by_email(conn=conn, email=email)
 
-    async def update_user_email(self, conn, user_id: str, new_email: EmailStr):
-        count = await self.repo.update_user_email(
-            conn=conn, user_id=user_id, new_email=new_email
+    async def update_user_profile(
+        self, conn, user_id: str, email: EmailStr | None, full_name: str | None
+    ):
+        count = await self.repo.update_user_profile(
+            conn=conn, user_id=user_id, email=email, full_name=full_name
         )
 
         if count == 0:
             raise UserNotFoundException(user_id)
-        logger.info(f"Updated user {user_id} with new email {new_email}")
+        logger.info(
+            f"Updated user {user_id} with email {email} and full name {full_name}"
+        )
         return count
 
     async def update_user_password(
@@ -65,14 +71,21 @@ class UserService:
             await self.task_repo.delete_user_tasks(conn=conn, user_id=user_id)
         logger.info(f"Cleared library for user {user_id}")
 
-    async def update_user_preferences(self, conn, user_id: str, email_on_import_complete: bool, websocket_auto_reconnect: bool):
-        count = await self.user_preference_repo.update_user_preferences(
-            conn=conn, user_id=user_id, email_on_import_complete=email_on_import_complete, websocket_auto_reconnect=websocket_auto_reconnect
+    async def update_user_preferences(
+        self,
+        conn,
+        user_id: str,
+        email_on_import_complete: bool | None,
+        websocket_auto_reconnect: bool | None,
+    ):
+        return await self.user_preference_repo.update_user_preferences(
+            conn=conn,
+            user_id=user_id,
+            email_on_import_complete=email_on_import_complete,
+            websocket_auto_reconnect=websocket_auto_reconnect,
         )
-        if count == 0:
-            raise UserNotFoundException(user_id)
-        logger.info(f"Updated user preferences for user {user_id}")
-        return count
 
     async def get_user_preferences(self, conn, user_id: str):
-        return await self.user_preference_repo.get_user_preferences(conn=conn, user_id=user_id)
+        return await self.user_preference_repo.get_user_preferences(
+            conn=conn, user_id=user_id
+        )
