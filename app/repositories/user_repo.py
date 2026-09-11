a043d1073c993b803b232629177a205060b8381f
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-11 20:53:13 +0100

    add: mark_email_verified repo method added

--- app/repositories/user_repo.py
+++ app/repositories/user_repo.py
@@ -6,7 +6,7 @@ from app.core.database import with_connection
 class UserRepository:
     @with_connection
     async def get_user_by_id(self, conn: asyncpg.Connection, id: str):
-        query = "SELECT id, email, full_name, role, is_deleted, created_at FROM users WHERE id = $1"
+        query = "SELECT id, email, full_name, role, email_verified, is_deleted, created_at FROM users WHERE id = $1"
 
         return await conn.fetchrow(query, id)
 
@@ -31,6 +31,13 @@ class UserRepository:
     ):
         query = "UPDATE users SET password = $1 WHERE id = $2"
         status_str = await conn.execute(query, new_password_hash, user_id)
+        operation, _, affected_row = status_str.rpartition(" ")
+        return int(affected_row)
+
+    @with_connection
+    async def mark_email_verified(self, conn: asyncpg.Connection, user_id: str):
+        query = "UPDATE users SET email_verified = TRUE WHERE id = $1"
+        status_str = await conn.execute(query, user_id)
         operation, _, affected_row = status_str.rpartition(" ")
         return int(affected_row)
 
