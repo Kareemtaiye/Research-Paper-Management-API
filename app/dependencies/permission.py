17b8a5aecfd5b80c21e3bcfce531c674962ed7aa
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-12 12:18:21 +0100

    fix: role bug

--- app/dependencies/permission.py
+++ app/dependencies/permission.py
@@ -36,7 +36,7 @@ class RequireRole:
         self.allowed_roles = allowed_roles
 
     def __call__(self, user: UserOutput = Depends(get_current_user)):
-        if user.role not in self.allowed_roles:
+        if user.role.lower() not in self.allowed_roles:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail=ErrorResponse(
